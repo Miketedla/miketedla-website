d8d006228270112da074cd14519d272c436bff4b
Michael tedla <mike@Mac.lan>
2026-07-19 20:09:22 +0200

    Add official branding and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 export const metadata: Metadata = {
   title: "Mike Tedla | DJ & Eventpartner",
   description:
@@ -23,11 +14,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-   <html lang="sv">
+    <html lang="sv">
       <body>{children}</body>
     </html>
   );
